17a57ff44faa8b41770a4f67679912d42c167702
FusTaFah <mfadel_1990@hotmail.com>
2016-10-19 18:54:23 +0100

    changes renderer
    Begin tutorial 2 by Niko Kauppi

--- FromScratchVulkan/Renderer.cpp
+++ FromScratchVulkan/Renderer.cpp
@@ -18,7 +18,7 @@ void Renderer::_InitInstance()
 {
 	VkApplicationInfo appInfo{};
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.apiVersion = VK_API_VERSION_1_0;
+	appInfo.apiVersion = VK_MAKE_VERSION(1, 0, 3);
 	appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
 	appInfo.pApplicationName = "Vulkan First";
 
